70b73087ef754bfe7aa7f3bf868636dd710b0408
e.palmisano <e.palmisano@reply.it>
2023-07-10 12:11:53 +0200

    fix: information disclosure

--- src/com/notsecurebank/servlet/LoginServlet.java
+++ src/com/notsecurebank/servlet/LoginServlet.java
@@ -58,7 +58,6 @@ public class LoginServlet extends HttpServlet {
             password = password.trim();
 
             if (!DBUtil.isValidUser(username, password)) {
-                LOG.error("Login failed >>> User: " + username + " >>> Password: " + password);
                 throw new Exception("Login Failed: We're sorry, but this username or password was not found in our system. Please try again.");
             }
         } catch (Exception ex) {
